5da44411be17238801af0663adcfac95fcfc4424
mehmet can yolar <mehmetcanyolar@gmail.com>
2024-10-27 21:52:45 +0300

    tables have been created, sample data sets have been created and logged, some panel design kicked off

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,39 @@ namespace Project03_EntityFrameworkStatistics
         {
             InitializeComponent();
         }
+
+       Db3Project20Entities db =new Db3Project20Entities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // toplam kategori sayısı 
+
+            int categoryCount = db.TblCategories.Count();
+
+            lblCategoryCount.Text = categoryCount.ToString();
+
+            //toplam ürün sayısı
+
+            int productCount = db.TblProducts.Count();
+
+            lblProductCount.Text = productCount.ToString();
+
+            // toplam müşteri sayısı
+
+            int customerCount = db.TblCustomers.Count();
+            lblCustomerCount.Text = customerCount.ToString();
+
+            // toplam Sipariş sayısı
+
+            int orderCount = db.TblOrders.Count();
+            lblOrderCount.Text = orderCount.ToString();
+
+            //toplam stok sayısı
+
+            var totalProductStockCount = db.TblProducts.Sum(x => x.ProductStock);
+            lblProductTotalStock.Text = totalProductStockCount.ToString();
+
+
+        }
     }
 }
